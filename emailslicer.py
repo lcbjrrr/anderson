66216b7100cf93226e2154d7ec4a7f2d0ddb2dcf
Andersonlima1 <anderson.lima@webera.com>
2020-07-21 17:26:37 -0300

    going

--- emailslicer.py
+++ emailslicer.py
@@ -3,3 +3,18 @@ user = email[:email.index("@")]
 domain = email[email.index("@") + 1 :]
 output = "Your username is {} and your domain name is {}".format(user,domain)
 print(output)
+## >, <, ==, >=, <=, !=
+
+
+
+
+##if condition:
+##    code 
+##elif condition2:
+ ##   code2 
+##elif condition3:
+  ##  code3
+  ##  else:
+ ##       code4 
+
+
