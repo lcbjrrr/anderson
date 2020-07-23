67a85d196ceb028a7742b6d890056c682db2116a
Andersonlima1 <anderson.lima@webera.com>
2020-07-23 09:26:05 -0300

    tradutor ok

---
+++ while.py
@@ -0,0 +1,23 @@
+number = 1
+while number<= 6000:
+    print(number)
+    number = number + 1
+   
+
+
+
+    ##intervalo de 2
+number = 1
+while number<= 6000:
+    if number %2 != 0:
+        print(number)
+    number = number + 1
+
+
+L = []
+while len(L)> 5:
+    new_name = ("por favor, adicione algum nome")
+    L.append(new_name)
+    print("Sorry, list is full")
+    print(L)
+    
